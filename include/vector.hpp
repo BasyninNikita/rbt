a9a6b7083ed85d45c76cf5221219059b354ae592
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2018-05-03 16:35:47 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -27,139 +27,39 @@ public:
 			delete node;
     		}
   	}
-	vector_t(vector_t const & other);
-	vector_t & operator =(vector_t const & other);
-	
-	std::size_t size() const;
-	std::size_t capacity() const;
-	T & at(std::size_t index);
-	void push_back(T value);
-	void pop_back();
-	T & operator [](std::size_t index);
-	T operator [](std::size_t index) const;
-	bool operator ==(vector_t const & other) const;
-};
-template <typename T>
-vector_t<T>::vector_t()
-{
-	size_ = 0;
-	capacity_ = 0;
-	elements_ = new T [capacity_];
-}
-template <typename T>
-vector_t<T>::vector_t(vector_t<T> const & other)
-{
-	size_ = other.size_;
-	capacity_ = other.capacity_;
-	elements_ = new T [capacity_];	
-	for (std::size_t i=0; i<other.capacity_; i++) {
-		elements_[i] = other.elements_[i];
-	}	
-}
-template <typename T>
-T & vector_t<T>::at(std::size_t index)
-	{
-		if (index >= size_)
-		{
-			throw std::out_of_range("Error");
+	void print(std::ostream & stream,int lvl,node_t* node){
+	if (node==nullptr)
+		return;
+		print(stream,lvl +1,node->right);
+			for (unsigned int i = 0; i < lvl; i++) {
+			stream << "---";
 		}
-		return (*this)[index];
+		stream << node->value <<std::endl;
+		print(stream, lvl + 1, node->left);
 	}
-template <typename T>
-vector_t<T> & vector_t<T>::operator =(vector_t const & other)
-{
-	size_=other.size_;
-	capacity_=other.capacity_;
-	elements_ = new T[capacity_];
-	for(std::size_t i=0;i<other.capacity_;i++)
-	{
-		elements_[i]=other.elements_[i];
+	bool  ravnbranch(node_t * first,node_t * second) const{
+		if((first==nullptr) && (second==nullptr)) return (true);
+		else if (first!=nullptr && second!=nullptr){
+				if((first->value==second->value)&&(ravnbranch(first->left,second->left)) && (ravnbranch(first->right,second->right)))
+					{return(true);}
+				else return(false);
+		}
+		else return(false);
 	}
-	return *this;
-}
-template <typename T>
-bool vector_t<T>::operator ==(vector_t const & other) const
-{
-	bool succ=false;
-	 if((size_==other.size_) && (capacity_==other.capacity_))
-	 {
-		 for(size_t i=0;i<other.size_;i++)
-		 {
-			 if(elements_[i]!=other.elements_[i])
-			 {
-				 return succ;
-			 }
-		 }
-		 succ=!succ;
-	 }
-	return succ;
-}
-template <typename T>
-vector_t<T>::~vector_t()
-{
-	delete []elements_;
-}
-template <typename T>
-std::size_t vector_t<T>::size() const
-{
-    return size_;
-}
-template <typename T>
-std::size_t vector_t<T>::capacity() const
-{
-    return capacity_;
-}
-template <typename T>
-void vector_t<T>::push_back(T value)
-{
-	if(capacity_==size_){
-		if(capacity_==0) 
-			capacity_=1;
-		else capacity_*=2;
-		T * p = new T [capacity_];
-        	for (std::size_t i=0; i<size_; i++) {
-            		p[i] = elements_[i];
-        	}
-        	delete [] elements_;
-        	elements_ = p;
+	auto operator==(tree_t & other) const{
+		node_t * first;
+		node_t * second;
+        	first = root_;
+        	second= other.root();
+        	return(ravnbranch(first, second));
 	}
-	elements_[size_++]=value;
+	tree_t(std::initializer_list<T> keys){
+		root_=nullptr;
+		int n = keys.size();
+		const int* _ptr = keys.begin();
+		for (int i=0; i < n; i++)
+		{
+			insert(_ptr[i]);
+		}
+	}
 	
-}
-template <typename T>
-void vector_t<T>::pop_back()
-{
-    if(size_==0) return;
-    size_--;
-    if( size_ == capacity_/4 ){
-        capacity_ /=2;
-        T * p = new T [capacity_];
-        for (std::size_t i=0; i<size_; i++) {
-            p[i] = elements_[i];
-        }
-        delete [] elements_;
-        elements_ = p;
-    }
-}
-template <typename T>
-T & vector_t<T>::operator [](std::size_t index)
-{
-	return elements_[index];
-}
-template <typename T>
-T vector_t<T>::operator [](std::size_t index) const
-{
-	return elements_[index];
-}
-template <typename T>
-bool operator !=(vector_t<T> const & lhs, vector_t<T> const & rhs)
-{
-	bool succ=true;
-	if(lhs==rhs)
-	{
-		succ=false;
-	}
-	return succ;
-}
-
-
